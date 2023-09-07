c4e953190a50b866348c761c6512e78bfa4e03e2
Christopher Arredondo <christopher.arredondo@mercadolibre.cl>
2023-09-06 21:09:46 -0300

    Initial Scaffolding

--- api/server.js
+++ api/server.js
@@ -16,29 +16,10 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-
-console.log(db.url);
-
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to application" });
 });
-
-require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
