226243226f10e16ae60532998eb5e4d4faa58a41
Christopher Arredondo <christopher.arredondo@mercadolibre.cl>
2023-09-07 09:22:14 -0300

    Included GET and POST Example

--- api/server.js
+++ api/server.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 
 const app = express();
 
+/*
 var corsOptions = {
   origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
+ */
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -18,7 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to application" });
+  res.json({
+    message: "Welcome to application",
+    data: ["NodeJS", "Express", "MongoDB", "Docker"]
+  });
+});
+
+app.post("/usuario", (req, res) => {
+  const { nombre, edad } = req.body;
+  res.send(`El nombre es: ${nombre} y la edad es: ${edad}`);
 });
 
 // set port, listen for requests
