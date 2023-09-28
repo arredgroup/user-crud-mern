bfe4ad39ed321660894446135708a797bb7ec7b8
Christopher Arredondo <christopher.arredondo@mercadolibre.cl>
2023-09-28 09:20:06 -0300

    Añade RUT a validación dentro del middleware

--- api/middlewares/user.middleware.js
+++ api/middlewares/user.middleware.js
@@ -1,5 +1,5 @@
 const validateRUT = (req, res, next) => {
-    const rut = req.params.rut;
+    const rut = req.params.rut || req.body.rut;
     if (!rut) {
         return res.status(400).json({ msg: 'El RUT es requerido' });
     }
