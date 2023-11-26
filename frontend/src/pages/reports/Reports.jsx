243102b2c5061e95e8dae3b5c4103ebb9402b8f1
Sebastián Gallegos F <59512190+SebaGallegos@users.noreply.github.com>
2023-11-26 17:59:49 -0300

    Reporte simple (total, entradas y salidas)
    Se agrega a la página un buscador por rut y devuelve la cantidad de marcaciones hechas por el usuario y su tipo (entrada y salida)

--- frontend/src/pages/reports/Reports.jsx
+++ frontend/src/pages/reports/Reports.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from "react-bootstrap";
+import { searchUserByRut } from '../../services/user.service';
+import { searchCheckByRut } from '../../services/check.service';
 
 const Reports = () => {
+    const [rut, setRut] = useState('');
+    const [usuario, setUsuario] = useState(null);
+    const [marcaciones, setMarcaciones] = useState(null);
+
+    const searchUser = () => {
+        searchUserByRut(rut).then(response => {
+            setUsuario(response.data);
+        });
+        searchCheckByRut(rut).then(response => {
+            setMarcaciones(response.data);
+        });
+    }
+
+    const entradas = marcaciones ? marcaciones.filter(marcacion => marcacion.tipo == 1).length : 0;
+    const salidas = marcaciones ? marcaciones.filter(marcacion => marcacion.tipo == 2).length : 0;
+
     return (
         <div>
             <h1>Reportes</h1>
+            <div className="form-group">
+                <label>Rut</label>
+                <input type="text" className="form-control" placeholder="22222222-2" value={rut} onChange={e => setRut(e.target.value)} />
+                <Button variant="primary" onClick={() => searchUser()}>Buscar</Button>
+            </div>
+            {marcaciones && (
+                marcaciones.length > 0 ? (
+                    <>
+                        <p>{`Total de marcaciones: ${marcaciones.length}`}</p>
+                        <p>{`Total de entradas: ${entradas}`}</p>
+                        <p>{`Total de salidas: ${salidas}`}</p>
+                    </>
+            ) : (
+                    <p>Sin marcaciones</p>
+                )
+            )}
         </div>
-    );
-};
+    )
+}
 
 export default Reports;
