2cc429ea7f5eb6bb2851b53d63b495cead3ae9f9
Hernan Barrales <hernanfelipe.barrales@alumnos.ulagos.cl>
2023-11-28 20:02:59 -0300

    const diasTrabajados
    Se agrego la constante diasTrabajados donde utilizamos la funcion reduce, que esta nos va a servir para contar la cantidad de días diferentes en los que se realizaron marcaciones y extrayendo las fechas únicas del array de marcaciones, la funcion includes sirve para verificar la fecha  (marcador.fecha) ya está en el array contador. Si no está, entonces la agrega con contador.push(marcador.fecha) y se obtiene la longitud del array contador con contador.length.

--- frontend/src/pages/reports/report.jsx
+++ frontend/src/pages/reports/report.jsx
@@ -4,7 +4,6 @@ import { searchUserByRut } from '../../services/user.service';
 import { searchCheckByRut } from '../../services/check.service';
 
 const Report = () => {
-
     const [rut, setRut] = useState('');
     const [usuario, setUsuario] = useState(null);
     const [marcaciones, setMarcaciones] = useState([]);
@@ -18,23 +17,29 @@ const Report = () => {
         });
     }
 
+    const diasTrabajados = marcaciones.reduce((contador, marcador) => {
+        if (!contador.includes(marcador.fecha)) {
+            contador.push(marcador.fecha);
+        }
+        return contador;
+    }, []).length;
+
     return (
-        <div> 
+        <div>
             <div className="form-group">
                 <label>Rut</label>
                 <input type="text" className="form-control" placeholder="22222222-2" value={rut} onChange={e => setRut(e.target.value)} />
                 <Button variant="primary" onClick={() => searchUser()}>Buscar</Button>
             </div>{
-                usuario ? 
+                usuario ?
                     <div>
                         <h1>Reporte</h1>
+                        <p>Días trabajados: {diasTrabajados}</p>
                     </div>
-                    : <h2>Sin Resultados</h2> 
+                    : <h2>Sin Resultados</h2>
                 }
         </div>
     );
-
 }
 
 export default Report;
-
