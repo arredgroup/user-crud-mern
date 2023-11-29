1aaf29656d781a8929a9b4334dd4449876a7e169
Hernan Barrales <hernanfelipe.barrales@alumnos.ulagos.cl>
2023-11-29 18:57:36 -0300

    Dias que se trabajo menos de 8 horas
    Se agrego un useEffect donde esta la logica del los dias que se trabajaron menos de 8 horas esto se hizo con el modulo moment

--- frontend/src/pages/reports/report.jsx
+++ frontend/src/pages/reports/report.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import {Button} from "react-bootstrap";
 import { searchUserByRut } from '../../services/user.service';
 import { searchCheckByRut } from '../../services/check.service';
+import moment from 'moment';
 
 const Report = () => {
     const [rut, setRut] = useState('');
     const [usuario, setUsuario] = useState(null);
     const [marcaciones, setMarcaciones] = useState([]);
+    const [menos8Horas, setMenos8Horas] = useState(0);
 
     const searchUser = () => {
         searchUserByRut(rut).then(response => {
@@ -24,6 +26,34 @@ const Report = () => {
         return contador;
     }, []).length;
 
+    useEffect(() => {
+        const marcacionesPorFecha = marcaciones.reduce((contador, marcador) => {
+            const fecha = marcador.fecha;
+            if (!contador[fecha]) {
+                contador[fecha] = [];
+            }
+            contador[fecha].push(marcador);
+            return contador;
+        }, {});
+    
+        const horasPorFecha = {};
+        for (const fecha in marcacionesPorFecha) {
+            const marcacionesDelDia = marcacionesPorFecha[fecha];
+            const entrada = marcacionesDelDia.find(marcador => marcador.tipo === 1);
+            const salida = marcacionesDelDia.find(marcador => marcador.tipo === 2);
+            if (entrada && salida) {
+                const horaEntrada = moment(entrada.hora, 'HH:mm:ss');
+                const horaSalida = moment(salida.hora, 'HH:mm:ss');
+                const horas = horaSalida.diff(horaEntrada, 'hours');
+                horasPorFecha[fecha] = horas;
+            }
+        }
+    
+        const menos8Horas = Object.values(horasPorFecha).filter(horas => horas < 8).length;
+        setMenos8Horas(menos8Horas);
+
+    }, [marcaciones]);
+
     return (
         <div>
             <div className="form-group">
@@ -35,6 +65,7 @@ const Report = () => {
                     <div>
                         <h1>Reporte</h1>
                         <p>Días trabajados: {diasTrabajados}</p>
+                        <p>Dias en que trabajó menos de ocho horas: {menos8Horas}</p>
                     </div>
                     : <h2>Sin Resultados</h2>
                 }
