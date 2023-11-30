d7853d1cd78e063214ab8ac33f56f8746096097e
A. Cid <lordix.acid@gmail.com>
2023-11-30 18:30:39 -0300

    Se agrega la información de los días con menos de 8 horas trabajadas y las horas extra

--- frontend/src/components/modalReport/modalReport.jsx
+++ frontend/src/components/modalReport/modalReport.jsx
@@ -5,16 +5,15 @@ import { searchCheckByRut } from '../../services/check.service';
 
 const ModalView = (props) => {
     const { user, showingModal, closeModal } = props;
-    const [reportData, setReportData] = useState([]);
 
     // Cantidad de días trabajados 
-    const [workedDays, setWorkedDays] = useState("");
+    const [workedDays, setWorkedDays] = useState(0);
 
     // Días en que el usuario trabajó menos de ocho horas
-    const [workedDaysLess, setWorkedDaysLess] = useState("");
+    const [workedDaysLess, setWorkedDaysLess] = useState(0);
 
     // Cantidad de horas extras trabajadas
-    const [extraHours, setExtraHours] = useState("");
+    const [extraHours, setExtraHours] = useState(0);
 
     const hideModal = () => {
         closeModal();
@@ -28,19 +27,62 @@ const ModalView = (props) => {
             }
         });
 
-        setWorkedDays(fechas.length)
+        setWorkedDays(fechas.length);
+    };
+
+    const countWorkedHours = (data) => {
+
+        let entradas = data.filter(el => el.tipo === 1);
+        let dias_trabajados = [];
+        let dias_horas_extra = [];
+        let horas_extra = 0;
+
+        entradas.map(elemento => {
+            let dia = data.filter(el => elemento.fecha === el.fecha);
+
+            if(dia.length < 2){
+                dias_trabajados.push(dia);
+            } else {
+                let fecha1 = new Date(dia[0].fecha + " " + dia[0].hora);
+                let fecha2 = new Date(dia[1].fecha + " " + dia[1].hora);
+
+                let segundos = Math.floor((fecha2 - fecha1) / 1000);
+                let minutos = Math.floor(segundos / 60);
+                let horas = Math.floor(minutos / 60);
+
+                if(horas < 8) {
+                    dias_trabajados.push(dia);
+                } else {
+                    let hrs_ext = horas - 8;
+                    horas_extra += hrs_ext;
+                    
+                    dias_horas_extra.push(
+                        {
+                            'horas_extras': hrs_ext,
+                            'dia': dia
+                        }
+                    );
+                }
+            }
+
+        });
+
+        setWorkedDaysLess(dias_trabajados.length);
+        setExtraHours(horas_extra);
+
+        console.log(dias_horas_extra)
     };
 
     useEffect(() => {
         if(showingModal) {
-            searchCheckByRut(user.rut).then((response)=>{
-                console.log(response.data.length);
+            searchCheckByRut(user.rut).then((response) => {
                 countWorkedDays(response.data);
+                countWorkedHours(response.data);
             }).catch((error)=>{
                 console.error(error);
             });
         }
-    }, [user.rut])
+    }, [user.rut, showingModal])
 
     return (
         <Modal show={showingModal} onHide={hideModal}>
@@ -49,7 +91,9 @@ const ModalView = (props) => {
             </Modal.Header>
             <Modal.Body>
                 <p>Usuario RUT: {user.rut}</p>
-                <p>Dias trabajados: {workedDays} Dias</p>
+                <p>Días trabajados: {workedDays} días</p>
+                <p>Días con menos de 8 horas trabajadas: {workedDaysLess} días</p>
+                <p>Horas extra trabajadas: {extraHours} horas</p>
             </Modal.Body>
             <Modal.Footer>
                 <button className="btn btn-secondary" onClick={hideModal}>Volver</button>
