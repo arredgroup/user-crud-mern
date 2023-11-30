82c6d9ccd5645bc3fa84153fd3efff7c6915bced
A. Cid <lordix.acid@gmail.com>
2023-11-30 20:37:55 -0300

    Se agrega tabla con los dias trabajados.

--- frontend/src/components/modalReport/modalReport.jsx
+++ frontend/src/components/modalReport/modalReport.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Modal from "react-bootstrap/Modal";
-// import { Table } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import { searchCheckByRut } from '../../services/check.service';
 
 const ModalView = (props) => {
     const { user, showingModal, closeModal } = props;
+    const [workedDaysData, setWorkedDaysData] = useState([])
 
     // Cantidad de días trabajados 
     const [workedDays, setWorkedDays] = useState(0);
@@ -33,15 +34,17 @@ const ModalView = (props) => {
     const countWorkedHours = (data) => {
 
         let entradas = data.filter(el => el.tipo === 1);
-        let dias_trabajados = [];
+        let dias_trabajados = []
+        let dias_menos_trabajados = [];
         let dias_horas_extra = [];
         let horas_extra = 0;
 
         entradas.map(elemento => {
             let dia = data.filter(el => elemento.fecha === el.fecha);
+            dias_trabajados.push(dia);
 
             if(dia.length < 2){
-                dias_trabajados.push(dia);
+                dias_menos_trabajados.push(dia);
             } else {
                 let fecha1 = new Date(dia[0].fecha + " " + dia[0].hora);
                 let fecha2 = new Date(dia[1].fecha + " " + dia[1].hora);
@@ -51,7 +54,7 @@ const ModalView = (props) => {
                 let horas = Math.floor(minutos / 60);
 
                 if(horas < 8) {
-                    dias_trabajados.push(dia);
+                    dias_menos_trabajados.push(dia);
                 } else {
                     let hrs_ext = horas - 8;
                     horas_extra += hrs_ext;
@@ -67,10 +70,16 @@ const ModalView = (props) => {
 
         });
 
-        setWorkedDaysLess(dias_trabajados.length);
+        setWorkedDaysLess(dias_menos_trabajados.length);
         setExtraHours(horas_extra);
+        setWorkedDaysData(dias_trabajados);
 
-        console.log(dias_horas_extra)
+        console.log(dias_trabajados)
+    };
+
+    const localeDate = (fecha) => {
+        let fecha_ = new Date(fecha);
+        return fecha_.toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' });
     };
 
     useEffect(() => {
@@ -78,6 +87,7 @@ const ModalView = (props) => {
             searchCheckByRut(user.rut).then((response) => {
                 countWorkedDays(response.data);
                 countWorkedHours(response.data);
+
             }).catch((error)=>{
                 console.error(error);
             });
@@ -85,15 +95,33 @@ const ModalView = (props) => {
     }, [user.rut, showingModal])
 
     return (
-        <Modal show={showingModal} onHide={hideModal}>
+        <Modal show={showingModal} onHide={hideModal} size="lg">
             <Modal.Header>
                 <Modal.Title>Reporte de Usuario</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <p>Usuario RUT: {user.rut}</p>
+                <p>Usuario RUT {user.rut}</p>
                 <p>Días trabajados: {workedDays} días</p>
+
                 <p>Días con menos de 8 horas trabajadas: {workedDaysLess} días</p>
                 <p>Horas extra trabajadas: {extraHours} horas</p>
+                <Table striped bordered hover size="sm">
+                    <tr>
+                        <th>Día</th>
+                        <th>Hora de entrada</th>
+                        <th>Hora de salida</th>
+                    </tr>
+                    
+                    {workedDaysData.map((elemento, indice) => {
+                        return <tr key={elemento[0].fecha + indice}>
+                        <td>{localeDate(elemento[0].fecha)}</td>
+                        <td>{elemento[0].hora}</td>
+                        <td>{elemento[1] ? elemento[1].hora: '---'}</td>
+                        </tr>
+                    })}
+                        
+                    
+                </Table>
             </Modal.Body>
             <Modal.Footer>
                 <button className="btn btn-secondary" onClick={hideModal}>Volver</button>
