8ec9b708c015f6ab76ba5af8d1b9e5c115a3c0fe
Psiloung <diegoypicho@gmail.com>
2023-11-30 23:52:30 -0300

    Cambio descartado

--- frontend/src/components/modalReport/modalReport.jsx
+++ frontend/src/components/modalReport/modalReport.jsx
@@ -1,35 +1,60 @@
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import { Table, Tag } from "antd"; 
+import { Table } from "react-bootstrap";
+//import { getAllChecksByRut } from '../../services/check.service';
+import { getReportData } from "../../../../controllers/check.controller";
 
-const { Column } = Table;
+const ModalReport = (props) => {
+    const { user, showingModal, closeModal } = props;
+    const [reportData, setReportData] = useState(null);
 
-const ReportModal = ({ data, showingModal, closeModal }) => {
-  return (
-    <Modal show={showingModal} onHide={closeModal} size="lg">
-      <Modal.Header closeButton>Reporte</Modal.Header>
-      <Modal.Body>
-        <BarChart width={500} height={300} data={data}>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="value" fill="#8884d8" />
-        </BarChart>
+    useEffect(() => {
+        if (showingModal && user) {
+            // Llama a la función getReportData aquí
+            getReportData(user.rut)
+                .then(response => {
+                    // Maneja la respuesta y actualiza el estado según sea necesario
+                    const reportData = response.data;
+                    setReportData(reportData);
+                })
+                .catch(error => console.error(error));
+        }
+    }, [showingModal, user]);
 
-        <Table dataSource={data}>
-          <Column title="Name" dataIndex="name" key="name" />
-          <Column title="Value" dataIndex="value" key="value" />
-        </Table>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={closeModal}>
-          Cerrar
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    const hideModal = () => {
+        closeModal();
+    };
+
+    return (
+        <Modal show={showingModal} onHide={hideModal}>
+            <Modal.Header>
+                <Modal.Title>Reporte de Asistencia y Horas Laborales</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {reportData ? (
+                    <Table striped bordered hover>
+                        <tr>
+                            <th>Días Trabajados</th>
+                            <td>{reportData.daysWorked}</td>
+                        </tr>
+                        <tr>
+                            <th>Días Menos De Ocho Horas</th>
+                            <td>{reportData.daysLessThan8Hours}</td>
+                        </tr>
+                        <tr>
+                            <th>Horas Extras</th>
+                            <td>{reportData.extraHoursWorked}</td>
+                        </tr>
+                    </Table>
+                ) : (
+                    <p>Cargando Reporte...</p>
+                )}
+            </Modal.Body>
+            <Modal.Footer>
+                <button className="btn btn-secondary" onClick={hideModal}>Volver</button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
-export default ReportModal;
+export default ModalReport;
